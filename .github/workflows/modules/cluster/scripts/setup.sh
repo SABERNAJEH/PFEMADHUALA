2def57116dbef336a7cf6e442d681a295903cb02
fraj-saber <157303732+fraj-saber@users.noreply.github.com>
2025-04-07 23:55:13 +0100

    Update setup.sh

--- .github/workflows/modules/cluster/scripts/setup.sh
+++ .github/workflows/modules/cluster/scripts/setup.sh
@@ -1,1 +1,12 @@
- setup
+#!/bin/bash
+set -e
+
+CLUSTER_NAME=$1
+echo "Setting up cluster: $CLUSTER_NAME"
+
+# Installation Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Démarrage du cluster
+minikube start -p $CLUSTER_NAME --driver=docker --memory=4096
