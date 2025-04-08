ad33404ea689f4136b30f4420f3736df9a13e873
BERCHINI <benmaktoumferjani@gmail.com>
2025-04-08 21:01:23 +0100

    Delete .github/workflows/modules directory

--- .github/workflows/modules/notification/scripts/send-report.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Sending security report..."
-# Logique d'envoi d'email/notification
--- .github/workflows/modules/visualization/scripts/deploy-monitoring.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Deploying monitoring stack..."
-# Commands d'installation Helm ici
